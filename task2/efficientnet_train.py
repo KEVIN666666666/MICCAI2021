9e1250562ba9845cb59ac4360ad06e9ea1374299
KEVIN666666666 <kevinfearcarot@gmail.com>
2021-07-25 22:36:01 +0800

    Update efficientnet_train.py

--- task2/efficientnet_train.py
+++ task2/efficientnet_train.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tqdm import tqdm
 from efficientnet_model import efficientnet_b7 as create_model
+from dataset import get_dataset, split_dataset
 import os
 
 
@@ -15,13 +16,14 @@ img_size = {"B0": 224,
 
 num_model = "B7"
 im_height = im_width = img_size[num_model]
-num_classes = 3
+num_classes = 2
 freeze_layers = True
-initial_lr = 0.01
+initial_lr = 1e-4  # 0.0001
 
 model = create_model(num_classes=num_classes)
 
 # load weights
+# TODO: Download pretrained model
 pre_weights_path = './efficientnetb7.h5'
 # assert os.path.exists(pre_weights_path), "cannot find {}".format(pre_weights_path)
 if os.path.exists(pre_weights_path):
@@ -38,7 +40,17 @@ if os.path.exists(pre_weights_path):
 
 model.summary()
 
-# TODO: read dataset
+# read dataset
+IMAGE_SIZE = im_height
+PATH = "../pre_data_V1/"
+label_file = "./task_2.txt"
+check = False
+
+data, label = get_dataset(PATH, IMAGE_SIZE, label_file, check)
+
+RATIO = 0.1
+SEED = 0
+train_X, train_Y, test_X, test_Y = split_dataset(data, label, RATIO, SEED)
 
 # TODO: training
 
